c7836d1d4457e9129721347b45c39b6d1ce43177
Shawn Kohn <shawnkohn@gmail.com>
2012-01-31 14:45:36 -0500

    refactored test file

--- game_spec.rb
+++ game_spec.rb
@@ -4,16 +4,25 @@ require './game'
 
 describe Game do
 	describe "#Score" do
+		before :each do
+			@game = Game.new
+		end
+
+		def rollMany(rolls, pins)
+			(1..rolls).each { @game.roll(pins) }
+		end
+		
 		it "score is 0 for an all gutter game" do
-			game = Game.new
-			20.times { game.roll(0) }
-			game.score.should == 0
+			20.times { @game.roll(0) }
+			@game.score.should == 0
 		end
 
 		it "score is 20 for rolling all ones" do
-			game = Game.new
-			20.times { game.roll(1) }
-			game.score.should == 20 
+			20.times { @game.roll(1) }
+			@game.score.should == 20 
 		end
+
+		#it "score is 16 for rolling one spare (5, 5, 3)" do
+		#end
 	end
 end
