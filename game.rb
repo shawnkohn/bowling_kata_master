28e74c682337aab03e17700fce91b6d042c252f6
Shawn Kohn <shawnkohn@gmail.com>
2012-01-31 14:27:02 -0500

    first passing test

---
+++ game.rb
@@ -0,0 +1,12 @@
+# game.rb
+
+class Game
+
+	def roll(pins)
+	end
+
+	def score
+		0
+	end
+
+end
--- game_spec.rb
+++ game_spec.rb
@@ -1,3 +1,13 @@
 # game_spec.rb
 
-describe
+require './game'
+
+describe Game do
+	describe "#Score" do
+		it "score is 0 for an all gutter game" do
+			game = Game.new
+			20.times { game.roll(0) }
+			game.score.should == 0
+		end
+	end
+end
